d684b3b9e79b78e03a5bc0f0e7f1f542b1ac72f5
Nasser Amer <nasser_amer09@yahoo.com>
2025-05-05 20:25:40 +0200

    created user mock tests

--- tests/test_user_mock.py
+++ tests/test_user_mock.py
@@ -2,10 +2,10 @@ import datetime
 import pytest
 from unittest.mock import MagicMock, patch
 from app.services.user_services import User_Services
-from models.users_model import User
+from app.models.users_model import User
 
 @patch('app.services.user_services.connectionToDataBase.DataBaseConnection.get_db_connection')
-def create_mock_test(mock_db_connection):
+def test_create_mock_test(mock_db_connection):
     services = User_Services()
 
     user_data={
@@ -30,7 +30,9 @@ def create_mock_test(mock_db_connection):
                                         "testusertest",
                                         'testlösenord', 
                                         'personal',
-                                        '0701234567')
+                                        '0701234567',
+                                        datetime.datetime.now() 
+                                        )
     
     user_obj, status_kod= services.createAccount(user_data)
 
@@ -39,7 +41,7 @@ def create_mock_test(mock_db_connection):
     assert user_obj.username == "testusertest"
 
 @patch('app.services.user_services.connectionToDataBase.DataBaseConnection.get_db_connection')
-def get_user_by_id_mock_test(mock_db_connection):
+def test_get_user_by_id_mock_test(mock_db_connection):
     service = User_Services()
 
     mock_conn = MagicMock()
@@ -63,20 +65,17 @@ def get_user_by_id_mock_test(mock_db_connection):
     assert result.username == "testusertest"
 
 @patch('app.services.user_services.connectionToDataBase.DataBaseConnection.get_db_connection')
-def get_all_users_mock_test(mock_db_connection):
+def test_get_all_users_mock_test(mock_db_connection):
     fake_users = [
     (1, "Alice", "Test", "alicetest", "admin", "hash1", "0700000001", datetime.datetime(2024, 1, 1)),
     (2, "Bob", "Demo", "bobdemo", "personal", "hash2", "0700000002", datetime.datetime(2024, 2, 1)),
     (3, "Charlie", "Example", "charlieex", "kund", "hash3", "0700000003", datetime.datetime(2024, 3, 1)),
     ]
 
-    
-
     mock_conn = MagicMock()
     mock_cursor = MagicMock()
     mock_conn.cursor.return_value = mock_cursor
     mock_db_connection.return_value = mock_conn
-
 
     mock_cursor.fetchall.return_value = fake_users
 
@@ -86,8 +85,49 @@ def get_all_users_mock_test(mock_db_connection):
     assert isinstance(result, list)
     assert len(result) ==  3
     for i, user in enumerate(result):
-        assert isinstance(result, User)
+        assert isinstance(user, User)
         assert user.username == fake_users[i][3]
 
+@patch('app.services.user_services.connectionToDataBase.DataBaseConnection.get_db_connection')
+def test_user_update_mock_test(mock_db_connection):
+    service = User_Services()
+    mock_conn = MagicMock()
+    mock_cursor=MagicMock()
+    mock_conn.cursor.return_value= mock_cursor
+    mock_db_connection.return_value = mock_conn
 
+    updated_data = {
+        "first_name": "UpdatedName",
+        "last_name": "UpdatedLast",
+        "role": "admin"
+    }
+
+    mock_cursor.fetchone.return_value = (
+        1,
+        "UpdatedName",
+        "UpdatedLast",
+        "testusertest",
+        "admin",
+        "hash123",
+        "0701234567",
+        datetime.datetime(2025, 1, 1)
+    )
+
+    result = service.update_user(1, updated_data)
+    assert isinstance(result, User)
+    assert result.username ==  "testusertest"
+    assert result.first_name == "UpdatedName"
+    assert result.role == "admin"
     
+
+@patch('app.services.user_services.connectionToDataBase.DataBaseConnection.get_db_connection')
+def test_delete_user_mock_test(mock_db_connection):
+    service = User_Services()
+    mock_conn = MagicMock()
+    mock_cursor=MagicMock()
+    mock_conn.cursor.return_value= mock_cursor
+    mock_db_connection.return_value = mock_conn
+
+    mock_cursor.rowcount = 1
+    result = service.delete_user(1)
+    assert result == True
